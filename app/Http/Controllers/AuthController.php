62048ff2ac93c52bc8490bb286a7dbabfa3385b4
Cyrelle <mascarinascyrelle@gmail.com>
2025-05-05 17:44:43 +0800

    updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,7 +36,7 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/');
+        return redirect()->route('login');
     }
 
     public function showRegisterForm()
